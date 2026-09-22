6fc392293ecb4800fbb4c2d745f1676c3a59ca8d
anshulbhullar21 <anshulbhullar@gmail.com>
2026-09-22 23:54:10 +0530

    if else logic questions

---
+++ python practice 2.py
@@ -0,0 +1,24 @@
+#1 if-else logic questions
+# n=int(input("enter a number : "))
+
+# if n%2==0:
+#     print("the number is even")
+# else:
+#     print("the number is odd")
+
+#2
+# n=int(input("enter a age : "))
+
+# if n>=18:
+#     print("person is eligible for vote")
+# else:
+#     print("person is not eligible for vote")
+
+#3
+
+
+
+
+
+
+
